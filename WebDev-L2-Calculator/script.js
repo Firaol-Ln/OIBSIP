8496264c714349370d401ba0ef10812ae9a7250d
Firaol Dereje <firaoldaraje@gmail.com>
2026-08-07 20:54:51 +0300

    start the js

---
+++ WebDev-L2-Calculator/script.js
@@ -0,0 +1,8 @@
+// Calculator variables
+let currentInput = "0";
+let previousInput = "";
+let operator = "";
+let newNumber = false;
+
+const current = document.getElementById("current");
+const history = document.getElementById("history");
