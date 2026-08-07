8d0f127c49c5c33dd29f29ef507274555f9e3052
Firaol Dereje <firaoldaraje@gmail.com>
2026-08-08 00:42:37 +0300

    display synchronized

--- WebDev-L2-Calculator/script.js
+++ WebDev-L2-Calculator/script.js
@@ -6,3 +6,14 @@ let newNumber = false;
 
 const current = document.getElementById("display-current");
 const history = document.getElementById("display-history");
+
+// Update the calculator screen
+function updateDisplay() {
+    current.textContent = currentInput;
+
+    if (previousInput !== "" && operator !== "") {
+        history.textContent = previousInput + " " + operator;
+    } else {
+        history.textContent = "";
+    }
+}
